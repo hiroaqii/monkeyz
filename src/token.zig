eb25b79e9880eaf31d9b01a2c8ec034fcf39f30d
hiroaqii <hiroaqii@gmail.com>
2025-05-31 23:04:42 +0900

    add TokenType and Token struct definitions

---
+++ src/token.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+
+pub const TokenType = enum {
+    ILLEGAL,
+    EOF,
+
+    // 識別子 + リテラル
+    IDENT, // add, foobar, x, y, ...
+    INT, // 1343456
+
+    // 演算子
+    ASSIGN,
+    PLUS,
+
+    // デリミタ
+    COMMA,
+    SEMICOLON,
+
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+
+    // キーワード
+    FUNCTION,
+    LET,
+
+    pub fn toString(self: TokenType) []const u8 {
+        return switch (self) {
+            .ILLEGAL => "ILLEGAL",
+            .EOF => "EOF",
+            .IDENT => "IDENT",
+            .INT => "INT",
+            .ASSIGN => "=",
+            .PLUS => "+",
+            .COMMA => ",",
+            .SEMICOLON => ";",
+            .LPAREN => "(",
+            .RPAREN => ")",
+            .LBRACE => "{",
+            .RBRACE => "}",
+            .FUNCTION => "FUNCTION",
+            .LET => "LET",
+        };
+    }
+};
+
+pub const Token = struct {
+    type: TokenType,
+    literal: []const u8,
+};
